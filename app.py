60768a741e71df334edb7ae443141eca03279fc1
Futa Hirakoba <korosuke613613@gmail.com>
2017-06-20 05:15:21 +0900

    Add Following event

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def handle_text_message(event):
 
 @handler.add(FollowEvent)
 def handle_following_message():
-        txt = "★宮大支援課お知らせBOT[非公式]" + "\n" + "このBOTは非公式のものです。宮崎大学とは一切関係ありません。" +  "\n" + "・'宮大'を送信すると直近5件のお知らせを表示します" + "\n" + "・'help'を送信するとこのメッセージを表示します"
+    txt = "★宮大支援課お知らせBOT[非公式]" + "\n" + "このBOTは非公式のものです。宮崎大学とは一切関係ありません。" +  "\n" + "・'宮大'を送信すると直近5件のお知らせを表示します" + "\n" + "・'help'を送信するとこのメッセージを表示します"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=txt)) #reply the same message from user
