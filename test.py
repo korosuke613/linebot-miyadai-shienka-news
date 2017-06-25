80c95d3cc1e9b4c042cd7d422031e433f1bc239e
Futa Hirakoba <korosuke613613@gmail.com>
2017-06-26 02:29:04 +0900

    Update README.md

--- test.py
+++
@@ -1,6 +0,0 @@
-from doco.client import Client
-c = Client(apikey='7430687741504d4c477771664a505a5a4a5749526e6a79526c734656346d51787a436c4d6469454e437231')
-user = { 'nickname': 'ふー', 'nickname_y': 'フー'}
-res = c.send(utt='好き', apiname='Dialogue', **user)
-print(res)
-print(res['utt'])
